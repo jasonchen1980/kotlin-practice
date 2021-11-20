a5ce76e40766fe1791d484a626e5c3954d8d0826
chairco <chairco@gmail.com>
2021-11-20 16:17:29 +0800

    build(compile): compile and install dependence tools

---
+++ target/kaptStubs/compile/lesson/Bmi.java
@@ -0,0 +1,21 @@
+package lesson;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0006\n\u0002\b\u0003\n\u0002\u0010 \n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0016\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u00042\u0006\u0010\u0006\u001a\u00020\u0004J\u0014\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\u00040\b2\u0006\u0010\t\u001a\u00020\n\u00a8\u0006\u000b"}, d2 = {"Llesson/Bmi;", "", "()V", "Caculate", "", "height", "weight", "CaculateMulti", "", "Bmis", "Llesson/Bmipair;", "mainModule"})
+public final class Bmi {
+    
+    public Bmi() {
+        super();
+    }
+    
+    public final double Caculate(double height, double weight) {
+        return 0.0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.Double> CaculateMulti(@org.jetbrains.annotations.NotNull()
+    lesson.Bmipair Bmis) {
+        return null;
+    }
+}
---
+++ target/kaptStubs/compile/lesson/Bmipair.java
@@ -0,0 +1,56 @@
+package lesson;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0006\n\u0000\n\u0002\u0010 \n\u0002\b\t\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u0086\b\u0018\u00002\u00020\u0001B\u001b\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\f\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005\u00a2\u0006\u0002\u0010\u0006J\t\u0010\u000b\u001a\u00020\u0003H\u00c6\u0003J\u000f\u0010\f\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005H\u00c6\u0003J#\u0010\r\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\u000e\b\u0002\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005H\u00c6\u0001J\u0013\u0010\u000e\u001a\u00020\u000f2\b\u0010\u0010\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0011\u001a\u00020\u0012H\u00d6\u0001J\t\u0010\u0013\u001a\u00020\u0014H\u00d6\u0001R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0017\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00030\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u0015"}, d2 = {"Llesson/Bmipair;", "", "height", "", "weights", "", "(DLjava/util/List;)V", "getHeight", "()D", "getWeights", "()Ljava/util/List;", "component1", "component2", "copy", "equals", "", "other", "hashCode", "", "toString", "", "mainModule"})
+public final class Bmipair {
+    private final double height = 0.0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.lang.Double> weights = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final lesson.Bmipair copy(double height, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.Double> weights) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object p0) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public Bmipair(double height, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.Double> weights) {
+        super();
+    }
+    
+    public final double component1() {
+        return 0.0;
+    }
+    
+    public final double getHeight() {
+        return 0.0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.Double> component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.Double> getWeights() {
+        return null;
+    }
+}
